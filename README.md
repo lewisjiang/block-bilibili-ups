17e1ff7d0eddb6b591187f0607a42945130b8f8f
lewisjiang <reg_sp@outlook.com>
2022-02-04 11:43:00 +0800

    adapt for new webpage

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # block-bilibili-ups
-A tempermonkey script to block videos from certain Bilibili ups.
+A tampermonkey script to block videos from certain Bilibili ups.
+一个在首页, 热门, 搜索页屏蔽特定B站up主视频的油猴脚本, aka用户的自发限流.
---
+++ script.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         Block Bilibili Up
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  通过up主用户名屏蔽B站Up
+// @author       Boku
+// @match        *://*.bilibili.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addElement
+// @grant        GM_xmlhttpRequest
+// @require      https://cdn.staticfile.org/jquery/3.4.1/jquery.min.js
+// @run-at document-end
+
+// ==/UserScript==
+/* eslint-env jquery */
+(function() {
+
+    //在这里输入你想屏蔽的Up名字
+    let disableUpList = [
+        "bbb",
+        "aaa"
+    ];
+
+    let diableUp = function(){
+        disableUpList.forEach(function(value,index,array){
+            let containsString = ":contains('" + value + "')";
+
+            // 屏蔽搜索页面
+            $("a.up-name" + containsString).parents('li.video-item').hide();
+
+            // 屏蔽主页面
+            // $("a.ex-up" + containsString).parents("div.video-card-common").hide();
+            $("span.bili-video-card__info--author" + containsString).parents("div.bili-video-card").remove();
+
+            // 屏蔽排行榜
+            // $("span.name" + containsString).parents("div.rank-wrap").hide();
+            $("span.up-name" + containsString).parents("li.rank-item").remove();
+
+            // 屏蔽热门页面
+            $("span.up-name__text" + containsString).parents("div.video-card").remove();
+        });
+    }
+
+    //diableUp();
+    let count = 0;
+    document.addEventListener('DOMNodeInserted', function() {
+        diableUp();
+    }, false);
+})();
+
+// 原作者：末夜の十字 https://www.bilibili.com/read/cv13500871
